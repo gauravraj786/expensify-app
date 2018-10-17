471f8430565376f691f877b30c770753db5c0a2e
gauravraj786 <grvbansal91@gmail.com>
2018-10-17 17:37:36 +0530

    Setup test database env

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DoTEnv from 'dotenv';
 
 Enzyme.configure({
     adapter: new Adapter()
 });
+
+DoTEnv.config({ path: '.env.test' });
